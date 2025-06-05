6cfab7a7ec55ffd6f5dc54943cbf553202cb73a3
Tenzin-blip <dolkertenzin9@gmail.com>
2025-06-05 13:05:57 +0545

    Modified gitignore

---
+++ plugins/example-plugin/example-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Example plugin
+ * Description: This is my first example plugin 
+ * Version: 1.0
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
