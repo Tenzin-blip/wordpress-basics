4ec49bdb7ac48cc97898c0c771d81900adb3ad4b
Tenzin-blip <dolkertenzin9@gmail.com>
2025-06-06 21:07:24 +0545

    Installed internationalization theme
    Updated git ignore

---
+++ themes/internationalization/assets/theme.js
@@ -0,0 +1,7 @@
+const __= wp.i18n__;
+/**
+ * Add event listener to the button
+ */
+document.querySelector( '#internationalization-settings-button' ).addEventListener( 'click', function(){
+    alert( 'Settings button clicked', 'internationalization' );
+} );
